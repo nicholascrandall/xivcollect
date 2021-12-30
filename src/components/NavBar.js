73992b1a35085412675fffdb829e32186c953bfa
Nicholas <nickycrandall@gmail.com>
2021-12-30 17:35:34 -0500

    slight reformatting

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const NavBar = () => {
+export default function NavBar() {
     return (
         <div className='navBar'>
             <h1>XIV Collect</h1>
         </div>
     )
 }
-
-export default NavBar
