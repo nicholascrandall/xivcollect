3797d067ce9bd7cc071f993a3ba5cee372f1c0d5
Nicholas <nickycrandall@gmail.com>
2021-12-30 17:01:37 -0500

    changed App from function to a component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+      </div>
+    )
+  }
+
 }
 
 export default App;
