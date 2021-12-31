a05d6b8e90a97aee28104aff4f9d25a859110119
Nicholas <nickycrandall@gmail.com>
2021-12-31 16:38:07 -0500

    setup xivapi fetching on item page

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react'
 import NavBar from './NavBar'
 
 export default function Item() {
+    const [item, setItem] = useState("")
+
+    useEffect(() => {
+        const url = "https://xivapi.com/item/1675"
+
+        const fetchData = async() => {
+            try {
+                const response = await fetch(url, {mode: 'cors'})
+                const json = await response.json()
+                console.log(json);
+                // setItem(json.item.etc)
+            } catch (error) {
+                console.log("error", error);
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    console.log(item)
+
     return (
         <div>
             <NavBar />
