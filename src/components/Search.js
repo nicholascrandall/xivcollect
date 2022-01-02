88a4d62ae93a497eb6ee917575d9a77c69bb2d81
Nicholas <nickycrandall@gmail.com>
2022-01-02 16:56:14 -0500

    added form handling functions

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,31 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export default function Search() {
-    const [itemID, setItemID] = useState("")
+    const [itemID, setItemID] = useState(1675)
+    const [searched, setSearched] = useState(false)
+
+
+    const handleChange = (event) => {
+        event.persist();
+        setItemID(event.target.value)
+        console.log(itemID)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setSearched(true)
+    }
+
+    if (searched === true) {
+        return <div><h2>SEARCHED</h2></div>
+    }
 
     return (
         <div className="searchContainer">
             <h2>Search Bar goes here</h2>
+            <form>
+
+            </form>
         </div>
     )
 }
