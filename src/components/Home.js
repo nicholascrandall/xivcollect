279208e57c65e2f4dc250ede590456ef03932f51
Nicholas <nickycrandall@gmail.com>
2022-01-02 16:16:02 -0500

    created search file, added to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import NavBar from './NavBar'
+import Search from './Search'
 
 export default function Home() {
     return (
         <div>
             <NavBar />
             <h2>Welcome to the home page!</h2>
+            <Search />
         </div>
     )
 }
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react'
+
+export default function Search() {
+    const [itemID, setItemID] = useState("")
+
+    return (
+        <div className="searchContainer">
+            <h2>Search Bar goes here</h2>
+        </div>
+    )
+}
