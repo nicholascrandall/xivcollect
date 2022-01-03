bc0e0c6b1afbf631ed54735baf6c9075a5fb3591
Nicholas <nickycrandall@gmail.com>
2022-01-02 20:09:42 -0500

    search data fetches and logs to the console

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react'
+
 export default function Results(props) {
-    console.log(props.term)
+    const [results, setResults] = useState("")
 
-    
+    useEffect(() => {
+        const url = `https://xivapi.com/search?string=${props.term}`
+        console.log(url)
 
-    return (
-        <h1>Search Results</h1>
-    )
+        const fetchData = async() => {
+            try {
+                const response = await fetch(url, {mode: 'cors'})
+                const json = await response.json()
+                console.log(json);
+                setResults(json)
+            } catch (error) {
+                console.log("error", error);
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    if (results.length < 1) {
+        return (
+            <div>
+                <h1>Loading Results...</h1>
+            </div>
+        )
+    } else {
+        return (
+            <h1>Search Results</h1>
+        )
+    }
 }
